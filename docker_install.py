1a9fdeb1b47df8dabb4df5963e2f656f9c633249
unknown <SuhaniArora>
2020-05-14 04:05:13 +0530

    docker

--- docker_install.py
+++ docker_install.py
@@ -7,5 +7,5 @@ baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/
 gpgcheck=0
 """)
 file.close()
-os.system("yum update")
+os.system("yum update -y")
 os.system("yum install docker-ce --nobest -y")
